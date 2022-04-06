12256fda527f871efb02b6da074d37fcae89e3a6
Dariusz Kowalczyk <darton@users.noreply.github.com>
2022-04-06 14:29:39 +0200

    Update postfix_map.sh

--- postfix_map.sh
+++ postfix_map.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-EXLIST_DIR=/home/ex2k
-SCRIPT_DIR=/root/scripts
-POSTFIX_DIR=/etc/postfix
-FILENAME=email.txt
-FILEHASH=email.hash
+SCRIPT_DIR=/root/skrypty
+EXLIST_DIR=/home/user/exchange
+FILENAME=email_exchangehostname.txt
+FILEHASH=email_exchangehostname.hash
 
+POSTFIX_DIR=/etc/postfix
+POSTFIX_RELAY_RECIPIENTS_FILE=relay_recipients
+RELAY_RECIPIENTS_FILE=recipients
+
+NOT_ALLOWED_DOMAINS_FILE=notallowed_domains
+ALLOWED_DOMAINS_FILE=allowed_domains
+NOT_ALLOWED_EMAILS_FILE=notallowed_emails
+
+POSTFIX_MAP_LOGFILE=/var/log/postfix_map.log
 
 function extract_valid_recipients {
-
-cat $1 |tr -d \" | tr , \\n| tr \; \\n| tr -d '\r' | awk -F\: '/(SMTP|smtp):/ {printf("%s\tOK\n" ,$2)}' | tr -d '}' | grep -v -f $SCRIPT_DIR/blacklist > $2
-
+    awk -F\" '{if(NR>2 && NF>1) print $2}' $1 \
+        |grep -f $SCRIPT_DIR/$ALLOWED_DOMAINS_FILE |grep -f $SCRIPT_DIR/$NOT_ALLOWED_EMAILS_FILE -v \
+        |grep -v -f $SCRIPT_DIR/$NOT_ALLOWED_DOMAINS_FILE \
+        |sort -u > $2
 }
 
+current_time=$(date '+%Y-%m-%d %H:%M:%S')
 
-function extract_valid_domains {
-
-cat $1 |tr -d \" | tr , \\n| tr \; \\n| tr -d '\r' | awk -F\: '/(DomainName):/ {printf("%s\tOK\n" ,$2)}' | tr -d '}' | grep -v -f $SCRIPT_DIR/blacklist > $2
-
-}
-
+[[ -f $SCRIPT_DIR/$ALLOWED_DOMAINS_FILE ]] || touch $SCRIPT_DIR/$ALLOWED_DOMAINS_FILE
+[[ -f $SCRIPT_DIR/$NOT_ALLOWED_DOMAINS_FILE ]] || touch $SCRIPT_DIR/$NOT_ALLOWED_DOMAINS_FILE
+[[ -f $SCRIPT_DIR/$NOT_ALLOWED_EMAILS_FILE ]] || touch $SCRIPT_DIR/$NOT_ALLOWED_EMAILS_FILE
 
 if [ ! -s $EXLIST_DIR/$FILEHASH ] || [ ! -s $EXLIST_DIR/$FILENAME ] ; then
 
@@ -29,41 +36,45 @@ if [ ! -s $EXLIST_DIR/$FILEHASH ] || [ ! -s $EXLIST_DIR/$FILENAME ] ; then
     if [ -f $EXLIST_DIR/$FILENAME ] ; then
         rm $EXLIST_DIR/$FILENAME
     fi
-    
-    echo "File does not exits or is empty!"
+
+    echo "$current_time -File $EXLIST_DIR/$FILEHASH or $EXLIST_DIR/$FILENAME does not exits or is empty!"
     exit 0
 fi
-
-#[[ -s $EXLIST_DIR/$FILENAME ]] || exit 0
-#
-#while [ ! -s $EXLIST_DIR/$FILEHASH ]; do
-#:
-#done
 
 HASH1=$(cat $EXLIST_DIR/$FILEHASH|awk '{print $1}'|sed '1s/^\xEF\xBB\xBF//'|tr -d '\r')
 HASH2=$(sha256sum $EXLIST_DIR/$FILENAME|awk '{print $1}'|tr a-z A-Z)
 
 if [ "$HASH1" == "$HASH2" ]; then
 
-    extract_valid_recipients $EXLIST_DIR/$FILENAME $SCRIPT_DIR/relay_recipients
-    extract_valid_domains $EXLIST_DIR/$FILENAME $SCRIPT_DIR/relay_domains
+    extract_valid_recipients $EXLIST_DIR/$FILENAME $SCRIPT_DIR/$RELAY_RECIPIENTS_FILE
 
     if [ $? == 0 ]; then
 
-        cat $SCRIPT_DIR/relay_recipients |awk '{print $1}' |sort -u | tr 'A-Z' 'a-z' | awk '{print $1"\tOK"}' > $POSTFIX_DIR/relay_recipients
+        if [ -s "$SCRIPT_DIR/$RELAY_RECIPIENTS_FILE" ]; then
+            cat $SCRIPT_DIR/$RELAY_RECIPIENTS_FILE |sort -u |awk -F"@" '{print $0"\tOK"}' > $POSTFIX_DIR/$POSTFIX_RELAY_RECIPIENTS_FILE
+        fi
 
-        [[ ! -f $EXLIST_DIR/$FILEHASH ]] || rm -f $EXLIST_DIR/$FILEHASH
+        if [ -s "$POSTFIX_DIR/$POSTFIX_RELAY_RECIPIENTS_FILE" ]; then
+            /usr/sbin/postmap hash:$POSTFIX_DIR/$POSTFIX_RELAY_RECIPIENTS_FILE
+            echo "$current_time - New relay_recipients was added to Postfix" | tee -a $POSTFIX_MAP_LOGFILE
+        else
+            echo "$current_time - File $POSTFIX_DIR/relay_recipients does not exits or is empty!" | tee -a $POSTFIX_MAP_LOGFILE
+        fi
 
-        /usr/sbin/postmap hash:$POSTFIX_DIR/relay_recipients
-
-        rm -f $EXLIST_DIR/$FILENAME
+        git_log
 
         echo "Done!"
     else
-        echo "Can not extract valid recipients from $EXLIST_DIR/$FILENAME"
-        exit 0
+        echo "Can not extract valid recipients from $EXLIST_DIR/$FILENAME" | tee -a $POSTFIX_MAP_LOGFILE
+
     fi
 else
-    echo "File does not exist, is empty or downloaded File checksum does not match!"
+    echo "File does not exist, is empty or downloaded File checksum does not match!" | tee -a $POSTFIX_MAP_LOGFILE
 
 fi
+
+[[ ! -f $EXLIST_DIR/$FILEHASH ]] || rm -f $EXLIST_DIR/$FILEHASH
+[[ ! -f $EXLIST_DIR/$FILENAME ]] || rm -f $EXLIST_DIR/$FILENAME
+[[ ! -f $SCRIPT_DIR/$RELAY_RECIPIENTS_FILE ]] || rm -f $SCRIPT_DIR/$RELAY_RECIPIENTS_FILE
+
+exit 0
